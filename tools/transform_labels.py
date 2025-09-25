25b8e257d6bf5b04464cd190caf96941228ff607
Matt Haberland <matt.haberland@gmail.com>
2025-09-25 11:40:20 -0700

    chore: run ruff format

--- tools/transform_labels.py
+++ tools/transform_labels.py
@@ -1,17 +1,28 @@
 """
 Transform labels to JSON compatible list to be used in GitHub actions with https://github.com/actions/labeler
 """
+
 import json
 import os
 
-DEFAULT_PACKAGES = ["deltakit-explorer", "deltakit-circuit", "deltakit-core", "deltakit-decode"]
+DEFAULT_PACKAGES = [
+    "deltakit-explorer",
+    "deltakit-circuit",
+    "deltakit-core",
+    "deltakit-decode",
+]
 
 
 def transform(labels_str: str) -> str:
     if not labels_str.strip():
         return json.dumps(DEFAULT_PACKAGES)
-    labels = [label.strip() for label in labels_str.split(",") if label.strip() in DEFAULT_PACKAGES]
+    labels = [
+        label.strip()
+        for label in labels_str.split(",")
+        if label.strip() in DEFAULT_PACKAGES
+    ]
     return json.dumps(labels)
+
 
 def main():
     all_labels = os.getenv("ALL_LABELS", "")
@@ -20,6 +31,7 @@ def main():
     with open(os.getenv("GITHUB_OUTPUT"), "a") as f:
         f.write(f"JSON_LABELS_ALL={transform(all_labels)}\n")
         f.write(f"JSON_LABELS_NEW={transform(new_labels)}\n")
+
 
 if __name__ == "__main__":
     main()
